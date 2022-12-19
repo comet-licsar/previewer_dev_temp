4bfae9ec0bc069bc9655ac84ba98136881ee09b6
laz048 <milan.lazecky@vsb.cz>
2022-12-19 00:07:30 +0000

    bangkok

--- panel_server_ext.py
+++ panel_server_ext.py
@@ -2,4 +2,6 @@ from subprocess import Popen
 # thanks to https://github.com/holoviz-demos/clifford
 
 def load_jupyter_server_extension(nbapp):
-    Popen(["panel", "serve", "preview1.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "bangkok.ipynb", "--allow-websocket-origin=*"])
+    #Popen(["panel", "serve", "preview1.ipynb", "--allow-websocket-origin=*"])
+    #panel serve --show --port 5009 bangkok.ipynb
